8a70c6bbfd5251a548fa70a00b9944cdd0101c6c
vasylashka <vasylashka@gmail.com>
2024-02-08 10:07:12 +0200

    Add contracts, dtos and mapper

---
+++ ToDoList.API/Contracts/Task/TaskCreateContract.cs
@@ -0,0 +1,11 @@
+﻿using ToDoList.Domain.Enums;
+
+namespace ToDoList.API.Contracts.Task;
+
+public class TaskCreateContract
+{
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public StatusTypes Status { get; set; }
+}
---
+++ ToDoList.Services/DTOs mapping/DtoToEntityMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ToDoList.Services.DTOs.Task;
+
+namespace ToDoList.Services.DTOs_mapping;
+
+public class DtoToEntityMapper : Profile
+{
+    public DtoToEntityMapper() 
+    {
+        TaskDtoToEntity();
+    }
+    private void TaskDtoToEntity()
+    {
+        CreateMap<TaskCreateDto, Domain.Entities.Task>();
+        CreateMap<TaskUpdateDto, Domain.Entities.Task>();
+    }
+}
